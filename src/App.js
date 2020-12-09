a60e062578ba0b7b4e8b38dd82672cc57c4624eb
10n9 <thanhlong6899@gmail.com>
2020-12-09 21:29:10 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import PublicNavbar from "./components/PublicNavbar";
+import HomePage from "./pages/HomePage";
+import ReadingPage from "./pages/ReadingPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import BookDetailPage from "./pages/BookDetailPage";
+import AlertMsg from "./components/AlertMsg";
+import CartPage from "./pages/CartPage";
+
+function App() {
+  return (
+    <Router>
+      <PublicNavbar />
+      <AlertMsg />
+      <Switch>
+        <Route exact path="/books/:id" component={BookDetailPage} />
+        <Route exact path="/reading" component={ReadingPage} />
+        <Route exact path="/cart" component={CartPage} />
+        <Route exact path="/" component={HomePage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
